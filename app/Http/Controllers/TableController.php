ed225b1881344e10ee4d3a3761c47fad5c457d17
jamil <jamil.cse13@gmail.com>
2021-08-30 20:09:59 +0600

    restore the json data file to storage

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -13,7 +13,6 @@ class TableController extends Controller
     public function index()
     {
         $path = storage_path()."/json/stock_market_data.json";
-        dd($path);
 
         $data = json_decode(file_get_contents($path), true);
         // $data = Http::get('https://api.npoint.io/6f468002bcd3ef2ab5ab')->json();
